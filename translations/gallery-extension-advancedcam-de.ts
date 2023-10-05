ff22a7075efd17b158e3f0c225b018c6ed1e2199
nephros <nemo@pgxperiiia10>
2023-10-05 16:00:29 +0200

    update l10n

--- translations/gallery-extension-advancedcam-de.ts
+++ translations/gallery-extension-advancedcam-de.ts
@@ -1,4 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+<context>
+    <name></name>
+    <message id="gallery-extension-advancedcam-title">
+        <source>Advanced Camera</source>
+        <extracomment>&quot;Advanced Camera&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="gallery-extension-advancedcam-source-title">
+        <source>Advanced Camera Photos</source>
+        <extracomment>&quot;Advanced Camera Photos&quot;</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
