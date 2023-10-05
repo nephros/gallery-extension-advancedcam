fe925f04a48025da09ed7912782f34bf7c69be94
nephros <nemo@pgxperiiia10>
2023-10-05 23:43:05 +0200

    l10n update

--- translations/gallery-extension-advancedcam-de.ts
+++ translations/gallery-extension-advancedcam-de.ts
@@ -1,25 +1,25 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name></name>
     <message id="gallery-extension-advancedcam-title">
         <location filename="../advancedcam/AdvCamMediaPage.qml" line="15"/>
         <source>Advanced Camera</source>
         <extracomment>&quot;Advanced Camera&quot;</extracomment>
-        <translation>Advanced Camera</translation>
+        <translation>Erweiterte Kamera</translation>
     </message>
     <message id="gallery-extension-advancedcam-title-photos">
         <location filename="../mediasource/AdvCamPhotoSource.qml" line="15"/>
         <source>Advanced Camera</source>
         <extracomment>&quot;Advanced Camera&quot;</extracomment>
-        <translation>Advanced Camera</translation>
+        <translation>Erweiterte Kamera Fotos</translation>
     </message>
     <message id="gallery-extension-advancedcam-title-videos">
         <location filename="../mediasource/AdvCamVideoSource.qml" line="15"/>
         <source>Advanced Camera Videos</source>
         <extracomment>&quot;Advanced Camera Videos&quot;</extracomment>
-        <translation>Advanced Camera Filme</translation>
+        <translation>Erweiterte Kamera Filme</translation>
     </message>
 </context>
 </TS>
