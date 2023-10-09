7a2c2500c34dc3797ed86d9b78c142aefdb0d84d
nephros <nemo@pgxperiiia10>
2023-10-09 13:00:56 +0200

    Update German translation

--- translations/gallery-extension-magic-de.ts
+++ translations/gallery-extension-magic-de.ts
@@ -1,20 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
+<context>
+    <name>EditSourcePage</name>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="72"/>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="79"/>
+        <location filename="../magic/editor/EditSourcePage.qml" line="106"/>
+        <source>Source Path</source>
+        <translation>Quellpfad</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="86"/>
+        <source>MagicSources Editor</source>
+        <translation>MagicSources Editor</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="95"/>
+        <source>Here you can add or remove media sources.</source>
+        <translation>Füge hier Medienquellen hinzu, oder entferne sie.</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="96"/>
+        <source>Pick a path from your home, give it a name and set it as either video, or picture source.</source>
+        <translation>Wähle einen Pfad in deinem Heimverzeichnis, benamse es, und definiere es als entweder Foto- oder Videoquelle.</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="99"/>
+        <source>Note that sources will only show up if their path exists.</source>
+        <translation>NB: Quellen erscheinen nur, wenn ihr Pfad auch existiert.</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="101"/>
+        <source>You will have to restart the app after changing anything.</source>
+        <translation>Du musst die App neu starten nachdem du etwas geändert hast.</translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="109"/>
+        <source>Display Name</source>
+        <translation>Anzeigename</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../magic/editor/EditSourcePage.qml" line="129"/>
+        <source>Picture Sources: %Ln</source>
+        <translation>
+            <numerusform>Bildquelle: eine</numerusform>
+            <numerusform>Bildquellen: %Ln</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../magic/editor/EditSourcePage.qml" line="142"/>
+        <source>Video Sources: %Ln</source>
+        <translation>
+            <numerusform>Videoquelle: eine</numerusform>
+            <numerusform>Videoquellen: %Ln</numerusform>
+        </translation>
+    </message>
+</context>
 <context>
     <name>MagicAlbumsPage</name>
     <message>
-        <location filename="../magic/MagicAlbumsPage.qml" line="16"/>
+        <location filename="../magic/MagicAlbumsPage.qml" line="22"/>
         <source>Magic Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Magic Alben</translation>
+    </message>
+    <message>
+        <location filename="../magic/MagicAlbumsPage.qml" line="54"/>
+        <source>Edit Sources</source>
+        <translation>Quellen bearbeiten</translation>
     </message>
 </context>
 <context>
     <name>MagicPhotoSource</name>
     <message>
-        <location filename="../mediasource/MagicPhotoSource.qml" line="43"/>
-        <source>Magic</source>
-        <translation type="unfinished"></translation>
+        <location filename="../mediasource/MagicPhotoSource.qml" line="18"/>
+        <source>MagicSources</source>
+        <translation>MagicSources</translation>
     </message>
 </context>
 </TS>
