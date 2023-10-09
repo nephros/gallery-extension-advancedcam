b2139448dc798daf14133b08afd7cc7aa4a14ce3
nephros <nemo@pgxperiiia10>
2023-10-09 13:01:47 +0200

    update l10n template file

--- translations/gallery-extension-magic-en.ts
+++ translations/gallery-extension-magic-en.ts
@@ -2,18 +2,81 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>EditSourcePage</name>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="72"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="79"/>
+        <location filename="../magic/editor/EditSourcePage.qml" line="106"/>
+        <source>Source Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="86"/>
+        <source>MagicSources Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="95"/>
+        <source>Here you can add or remove media sources.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="96"/>
+        <source>Pick a path from your home, give it a name and set it as either video, or picture source.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="99"/>
+        <source>Note that sources will only show up if their path exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="101"/>
+        <source>You will have to restart the app after changing anything.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/editor/EditSourcePage.qml" line="109"/>
+        <source>Display Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../magic/editor/EditSourcePage.qml" line="129"/>
+        <source>Picture Sources: %Ln</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../magic/editor/EditSourcePage.qml" line="142"/>
+        <source>Video Sources: %Ln</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
     <name>MagicAlbumsPage</name>
     <message>
-        <location filename="../magic/MagicAlbumsPage.qml" line="16"/>
+        <location filename="../magic/MagicAlbumsPage.qml" line="22"/>
         <source>Magic Albums</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../magic/MagicAlbumsPage.qml" line="54"/>
+        <source>Edit Sources</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MagicPhotoSource</name>
     <message>
-        <location filename="../mediasource/MagicPhotoSource.qml" line="43"/>
-        <source>Magic</source>
+        <location filename="../mediasource/MagicPhotoSource.qml" line="18"/>
+        <source>MagicSources</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
